510a1d988a907fc876120b64d0cbbc7543133cfd
root <root@localhost.localdomain>
2018-02-20 20:52:56 -0500

    Function fruit

--- fruit_basket.py
+++ fruit_basket.py
@@ -1,10 +1,19 @@
-fruit_basket = ["Tomato", "BlueBerry", "Apple", "Orange", "Pineapple"]
 
-guess = raw_input("Guess a fruit \n")
-if guess.isalpha():
-	if guess in fruit_basket:
-		print("You picked a fruit")
+def main():
+	fruit_basket = ["Tomato", "BlueBerry", "Apple", "Orange", "Pineapple"]
+	isWrong = False
+	while isWrong == False:
+		isWrong = guess_fruit(fruit_basket)
+
+def guess_fruit(basket):
+	guess = raw_input("Guess a fruit \n")
+	if guess.isalpha():
+		if guess in basket:
+			print("You picked a fruit")
+			return True
+		else:
+			print("You dind't pick a fruit")
+			return False
 	else:
-		print("You dind't pick a fruit")
-else:
-	print("Next time pick a string")
+		print("Next time pick a string")
+main()
